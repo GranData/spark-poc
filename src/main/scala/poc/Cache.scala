9622ce991b68368a9df3f7f3759ccb742dad1f4a
Esteban Donato <esteban.donato@gmail.com>
2015-07-21 17:09:49 -0300

    more cache examples

--- src/main/scala/poc/Cache.scala
+++ src/main/scala/poc/Cache.scala
@@ -6,36 +6,42 @@ import java.io.IOException
 import java.io.ObjectInputStream
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.HashPartitioner
+import org.apache.spark.storage.StorageLevel
 object Cache extends App {
-  System.clearProperty("spark.driver.port")
-  val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")//.set("spark.driver.port","")
+  //System.clearProperty("spark.driver.port")
+  val conf = new SparkConf()
+    .setAppName("Simple Application")
+    .setMaster("spark://esteban:7077")
+    .set("spark.eventLog.enabled", "true")
   val sc = new SparkContext(conf)
   Cache1.run(sc)
 }
 object Cache1 {
   
   def run(sc: SparkContext) = {
-   
-  val rdd = sc.parallelize(Array(1,2,3,4), 2)
+  try{ 
+  val rdd = sc.textFile("/home/esteban/data/numbers.txt")
   
-  val rdd1 = rdd.map { x =>
-    println("map " + x)
-    x*10
+  val rdd1 = rdd.mapPartitions { x =>
+    println("map rdd1, partition running on thread " + Thread.currentThread().getName)
+    x
   }.map { x => 
-    println("map2 " + x)
+    //println("map2 " + x)
     (x,x)
-  }.partitionBy(new HashPartitioner(3))//.reduceByKey(_+_)
-  //}
+  }.setName("rdd1").repartition(4).cache()
+  //rdd1.count()
   
   val rdd2 = rdd1.map { x =>
-    println("map rdd1 to rdd2")
+    //println("map rdd1 to rdd2")
     x
   }
-  //rdd1.cache()
   
-  rdd1.count()
-  rdd2.count()
-  sc.stop()  
+ 
+  
+  println("count!!!!!!!!!!!!!!!!!!! " + (rdd1 join rdd2).count())
+  //println("count!!!!!!!!!!!!!!!!!!! " + (rdd1 join rdd3).count())
+  }
+  finally sc.stop()  
   }
   
 }
